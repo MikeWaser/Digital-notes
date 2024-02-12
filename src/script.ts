f66d15f2db0438b7108a815279f8a190ef21000c
MikeWaser <mik3waser@gmail.com>
2024-02-12 17:25:25 +0100

    scss, html

--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,3 @@
-import '/src/style/style.css'
-
 async function getData() {
     const response = await fetch("https://o6wl0z7avc.execute-api.eu-north-1.amazonaws.com/api/notes/:username");
     console.log(response);
